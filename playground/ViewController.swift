ed5359b0f111b9981b65ea2112e45658bf85d4bc
Pedro <antonioaren@hotmail.com>
2018-08-07 14:57:09 +0100

    Playground to understand how its work

--- playground/ViewController.swift
+++ playground/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var waiting: UIActivityIndicatorView!
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        waiting.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            if(self.titleLabel.text=="Hello World") {
+                self.titleLabel.text="Hola Mundo"
+            } else {
+                self.titleLabel.text="Hello World"
+            }
+            self.waiting.stopAnimating()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        titleLabel.text="Hello World"
+        waiting.stopAnimating()
+        waiting.hidesWhenStopped=true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
